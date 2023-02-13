622240e161d28bac3a542ba6f9a9c5331983d3bf
Sanders76 <sanders76@list.ru>
2023-02-13 22:43:25 +0300

    Added code to Task_4.2_(24).py

--- Seminar_4_Homework/Task_4.2_(24).py
+++ Seminar_4_Homework/Task_4.2_(24).py
@@ -16,3 +16,26 @@
 
 # Input1: 11, 92, 1, 42, 15, 12, 11, 81
 # Output1: Макс. кол-во ягод 184, собрано для куста 1
+
+
+
+arrangement_of_bushes = [1, 2, 3, 4, 5, 6, 7, 8]
+# arrangement_of_bushes = [11, 92, 1, 42, 15, 12, 11, 81]
+
+fee_for_approach = []
+
+for bush in range(len(arrangement_of_bushes)):
+    if bush == 0:
+        fee_for_approach.append(arrangement_of_bushes[0] + arrangement_of_bushes[1] + arrangement_of_bushes[-1])
+    elif bush == (len(arrangement_of_bushes) - 1):
+        fee_for_approach.append(arrangement_of_bushes[-1] + arrangement_of_bushes[-2] + arrangement_of_bushes[0])
+    else:
+        fee_for_approach.append(arrangement_of_bushes[bush] + arrangement_of_bushes[bush - 1] + arrangement_of_bushes[bush + 1])
+
+# print(fee_for_approach)
+
+max_value_of_berry = max(fee_for_approach)
+
+max_is_on_bush = fee_for_approach.index(max_value_of_berry) + 1
+
+print(f'Макс. кол-во ягод {max_value_of_berry}, собрано для куста {max_is_on_bush}.')
